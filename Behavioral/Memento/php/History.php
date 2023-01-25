11fd2cd711fbdc9997cba5d1572dce419aef4e6d
imaarov <imanatarof@gmail.com>
2023-01-25 20:04:49 +0330

    complete memento pattern

--- Behavioral/Memento/php/History.php
+++ Behavioral/Memento/php/History.php
@@ -6,7 +6,25 @@ class History {
     
     private array $states;
 
+    /**
+     * Push some value in states array
+     * 
+     * @param BrowserState $browserState
+     * @return void
+     */
     public function push(BrowserState $browserState) {
         $this->states[] = $browserState;
+    }
+
+    /**
+     * Remove last element and return it.
+     * 
+     * @param void
+     * @return 
+     */
+    public function pop() {
+        $last_value = $this->states[count($this->states) - 1];
+        array_pop($this->states);
+        return $last_value;
     }
 }
--- Behavioral/Memento/php/main.php
+++ Behavioral/Memento/php/main.php
@@ -3,11 +3,30 @@
 include_once 'Browser.php';
 
 use DesignPattern\Creational\Php\Browser;
+use DesignPattern\Creational\Php\History;
 
 class Main {
 
-    public function __construct()
+    # Init the dependenies class (The history and state classes)
+    public function __construct(
+        private $browser = new Browser(),
+        private $history = new History()
+    )
+    { }
+
+    public function setDataToTab(string $data, bool $saveToHistory = true)
     {
-        $browser = new Browser();
+        # Set some data for tab
+        $this->browser->setTab("some");
+        # Create State and pass it to history
+        if($saveToHistory)
+            $this->history->push($this->browser->createState());
+        return true;
+    }
+
+    public function undo()
+    {
+        $this->browser->restore($this->history->pop());
+        return $this->browser->getTab();
     }
 }
